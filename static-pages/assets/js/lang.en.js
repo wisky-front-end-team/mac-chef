bdce7704a1ab5ee6396c79ba4d3049edb93f8c89
minhduc150496 <minhduc150496@gmail.com>
2017-03-08 13:56:11 +0700

    no caption

---
+++ static-pages/assets/js/lang.en.js
@@ -0,0 +1,13 @@
+if (typeof(lang) == 'undefined') {
+	lang = {}
+}
+
+lang.en = {
+	SignIn: 'Sign in',
+	Download: 'Donwload CATALOGUE',
+	ShopNow: 'Shop now',
+	TopProducts: 'Top products',
+	Customers: 'Customers'
+}
+
+console.log(lang);
---
+++ static-pages/assets/js/lang.vi.js
@@ -0,0 +1,12 @@
+if (typeof(lang) == 'undefined') {
+	lang = {}
+}
+
+lang.vi = {
+	SignIn: 'Đăng nhập',
+	Download: 'Tải về CATALOGUE',
+	ShopNow: 'Mua ngay',
+	TopProducts: 'Sản phẩm hàng đầu',
+	Customers: 'Khách hàng',
+	Address: '138/66 Trương Công Định, Phường 14, Quận Tân Bình, TP.HCM'
+}
